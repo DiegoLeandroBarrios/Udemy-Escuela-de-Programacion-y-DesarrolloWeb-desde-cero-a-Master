005a4872c0a9241e21c91e47394b514468b3fd73
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-08 23:37:31 -0300

    Casting de datos.

---
+++ introduccuinJavaScript/01-introduccion/casting-datos.js
@@ -0,0 +1,25 @@
+//conversion de datos
+
+// de entero a string
+
+let entero = 10;
+console.log(typeof entero);
+
+entero = String(entero);
+console.log(typeof entero);
+
+//string a entero
+
+entero = "45";
+console.log(typeof entero);
+
+entero = Number(entero);
+console.log(typeof entero);
+
+//string a float
+
+entero = "4.5";
+console.log(typeof entero);
+
+entero = parseFloat(entero);
+console.log(typeof entero);
