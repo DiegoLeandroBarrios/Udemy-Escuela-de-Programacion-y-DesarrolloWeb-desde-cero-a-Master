de2ef1866f8946288ca0a4bf75285719e1b44d4b
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-08 23:21:08 -0300

    fixed Variables.

--- introduccuinJavaScript/01-introduccion/variables.js
+++ introduccuinJavaScript/01-introduccion/variables.js
@@ -2,11 +2,17 @@
 //es devilmente tipado. Es dinamico
 
 let nombre = "Diego"; //espacio de memoria en el cual puede ser alterable, la variable Let es para que tome linea por linea si no esta definido salta un error.
-
 console.log(nombre);
 
 //modificamos
 
 nombre = "Leandro";
-
 console.log(nombre);
+
+//nombre de variables
+
+let nombreCompleto = "Diego Leandro"; //las variables no podemos indicarlas con numeros o simbolos al principio.
+
+//variables constantes
+
+const pais = "Argentina"; //las contantes no se pueden modificar el valor.
