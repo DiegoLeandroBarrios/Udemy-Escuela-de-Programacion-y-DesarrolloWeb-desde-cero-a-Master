dedae4a7d8d9bfb44d14954608de71d1248223f6
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-08 23:20:11 -0300

    Variables

---
+++ introduccuinJavaScript/01-introduccion/variables.js
@@ -0,0 +1,12 @@
+//Definicion de variabbles
+//es devilmente tipado. Es dinamico
+
+let nombre = "Diego"; //espacio de memoria en el cual puede ser alterable, la variable Let es para que tome linea por linea si no esta definido salta un error.
+
+console.log(nombre);
+
+//modificamos
+
+nombre = "Leandro";
+
+console.log(nombre);
