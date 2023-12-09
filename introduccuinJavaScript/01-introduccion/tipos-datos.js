0971a3721c3aa6731987743808f804d7941bdcd6
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-08 23:29:47 -0300

    Tipos de Datos.

---
+++ introduccuinJavaScript/01-introduccion/tipos-datos.js
@@ -0,0 +1,24 @@
+//tipos de datos
+//tipo de dato indefinido
+
+let indefinido;
+console.log(indefinido);
+
+//tipo Cadena
+
+let text = "Hola mundo";
+console.log(typeof text);
+
+//tipos numero
+
+let entero = 22;
+console.log(typeof entero);
+
+let decimal = 8.7;
+console.log(typeof decimal);
+
+//tipos booleanos: son logicos
+console.log(typeof true);
+console.log(typeof false);
+
+//la programacion en si es manipular datos para crear aplicaciones mas avanzadas.
