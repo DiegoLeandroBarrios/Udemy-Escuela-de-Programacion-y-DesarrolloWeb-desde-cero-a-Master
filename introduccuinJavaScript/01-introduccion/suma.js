ba0925846aa87a8f9cf4a3026f98cfb23e62acce
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-08 23:47:00 -0300

    Practica Suma.

---
+++ introduccuinJavaScript/01-introduccion/suma.js
@@ -0,0 +1,17 @@
+//Entrada de datos
+
+let a = prompt("ingrese un numero: "); //promp pide al usuario que añada algo por teclado en el navegador.
+let b = prompt("ingrese otro numero: "); //promp lo toma como caracter tipo string.
+
+//Casting de datos
+
+a = Number(a);
+b = Number(b);
+
+//Operacion
+
+let suma = a + b;
+
+//Salidad de datos
+
+document.write("La suma es: ", suma);
