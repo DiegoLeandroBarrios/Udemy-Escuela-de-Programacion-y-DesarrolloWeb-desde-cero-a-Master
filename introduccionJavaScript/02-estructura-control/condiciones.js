917fd0ebf8a4200c34e6cfb108cd4ab2ae12f67f
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-12 15:45:56 -0300

    Mejorando el if-else.

--- introduccionJavaScript/02-estructura-control/condiciones.js
+++ introduccionJavaScript/02-estructura-control/condiciones.js
@@ -1,10 +1,25 @@
 let n = 5,
   print = "";
 
-if (n % 2 == 0) {
-  print = `El numero ${n} es par.`;
+if (n !== 0) {
+  // si el numero no es igual a cero
+  if (n > 0) {
+    //si el numero es positivo
+    if (n % 2 === 0) {
+      //si el numero es par
+      print = `El numero ${n} es par POSITIVO.`;
+    } else {
+      print = `El numero ${n} es impar POSITIVO`;
+    }
+  } else {
+    if (n % 2 === 0) {
+      print = `El numero ${n} es par NEGATIVO.`;
+    } else {
+      print = `El numero ${n} es impar NEGATIVO`;
+    }
+  }
 } else {
-  print = `El numero ${n} es impar.`;
+  print = "el numero es NEUTRO.";
 }
 
 console.log(print);
