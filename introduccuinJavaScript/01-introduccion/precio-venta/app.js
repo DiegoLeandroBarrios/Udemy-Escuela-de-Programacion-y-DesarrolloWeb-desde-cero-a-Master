a398f033e7602b2dcd441a0a86359c20be5f63d0
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-12 15:14:14 -0300

    Mejora de salida de datos.

--- introduccuinJavaScript/01-introduccion/precio-venta/app.js
+++ introduccuinJavaScript/01-introduccion/precio-venta/app.js
@@ -18,4 +18,33 @@ pv = vv + igv;
 
 document.write("valor de venta: ", vv, "<br>");
 document.write("IGV: ", igv, "<br>");
-document.write("Precio de venta: ", pv);
+document.write("Precio de venta: ", pv, "<br>");
+
+//Mejoramos la parte de la salida de datos on acento grave para mostrar las variables.
+
+document.write(`Valor de venta: ${vv} <br>
+              IGV: ${igv} <br>
+              Precio de venta: ${pv}`);
+
+//otra forma para mejorar
+
+console.log(`Valor de venta: ${vv}
+            IGV: ${igv}
+            Precio de venta: ${pv}`);
+
+//otra manera
+document.write(`<pre>
+              Valor de venta: ${vv}
+              IGV:            ${igv}
+              Precio de venta:${pv} 
+              </pre>`);
+
+//otra forma en consola(caracteres especiales)
+console.log(
+  `Valor de venta: ${vv}\nIGV: ${igv}\nPrecio de venta: ${pv}`
+);
+
+//otra forma de consola alineacion(caracteres especiales)
+console.log(
+  `Valor de venta:\t${vv}\nIGV:\t\t\t${igv}\nPrecio de venta:\t${pv}`
+);
