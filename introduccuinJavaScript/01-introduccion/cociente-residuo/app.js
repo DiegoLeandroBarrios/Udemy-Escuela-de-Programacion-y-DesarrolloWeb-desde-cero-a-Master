1770e15390f75e8e56cfde13790a8e78007898f7
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-11 21:02:19 -0300

    Práctica División.

---
+++ introduccuinJavaScript/01-introduccion/cociente-residuo/app.js
@@ -0,0 +1,18 @@
+//variables
+
+let n1, n2, cociente, residuo;
+
+//entrada de datos input
+
+n1 = Number(prompt("ingrese numero entero!!!!"));
+n2 = Number(prompt("ingrese numero entero!!!!"));
+
+//proceso
+
+cociente = n1 / n2;
+residuo = n1 % n2;
+
+//salida de datos output
+
+document.write("cociente: " + cociente + "<br>");
+document.write("residuo: ", residuo + "<br>");
