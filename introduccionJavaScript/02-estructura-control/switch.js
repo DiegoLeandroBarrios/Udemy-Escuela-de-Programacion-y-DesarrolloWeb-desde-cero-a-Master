631332b33d728034074d9a134df2c49f0d146a95
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2023-12-12 16:25:37 -0300

    Practica switch vocal.

---
+++ introduccionJavaScript/02-estructura-control/switch.js
@@ -0,0 +1,21 @@
+let v = "A",
+  print = "";
+
+switch (v) {
+  case "a": //encadenamiento de casos.
+  case "A":
+  case "e":
+  case "E":
+  case "i":
+  case "I":
+  case "o":
+  case "O":
+  case "u":
+  case "U":
+    print = `${v} es vocal.`;
+    break;
+  default:
+    print`${v} no es vocal.`;
+    break;
+}
+console.log(print);
