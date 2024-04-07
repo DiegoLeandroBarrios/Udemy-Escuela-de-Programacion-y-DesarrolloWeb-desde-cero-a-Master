9fe795f802192ad32cd4c2e2caba57e61552f9c2
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-07 17:09:45 -0300

    While

---
+++ introduccionJavaScript/02-estructura-control/while.js
@@ -0,0 +1,20 @@
+let n = 2,
+  c = 0;
+
+while (c <= 10) {
+  console.log(`${n} x ${c} = ${n * c}`);
+  c++;
+}
+let texts = "javaScript";
+let estudiantes = ["alex", "Roel", "Juan", "Diego"];
+let i = 0;
+while (i < texts.length) {
+  console.log(texts[i]);
+  i++;
+}
+
+let s = 0;
+do {
+  console.log(`hola`);
+  s++;
+} while (s < 5);
