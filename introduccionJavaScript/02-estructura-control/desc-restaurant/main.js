5e2b0575dccdeb6fcc8ff0d6731fb2441088250f
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-09 15:05:33 -0300

    Practica 2 Restaurante

--- introduccionJavaScript/02-estructura-control/desc-restaurant/main.js
+++ introduccionJavaScript/02-estructura-control/desc-restaurant/main.js
@@ -2,6 +2,18 @@
 Enunciado: un restaurante ofrece un descuento del 10% para consumo de hasta s/. 100.00 y un
 descuento del 20% para consumos mayores, para ambos casos se aplica un impuesto del 19%.
 Determinar el monto defidescuento, el impuesto y el importe a pagar.
+Practica 02: Descuentos de un restaurante Parte 02
+Enunciado: debido a los excelentes resultados, el restaurante decide ampliar sus ofertas de acuerdo a la
+siguiente escala de consumo, ver la tabla. Determinar el monto del descuento, el importe del impuesto y el
+importe a pagar.
+• Consumo (S/.)
+• Hasta 100
+• Mayor a 100
+• Mayor a 200
+Descuento (%)
+10
+20
+30
 */
 //entrada de datos con funcion prompt
 
@@ -12,10 +24,15 @@ if (consumo <= 100) {
   //descuento del 10%
   datoDescuento = "10%";
   descuento = consumo * 0.1;
-} else if (consumo > 100) {
+} else if (consumo > 100 && consumo <= 200) {
   //descuento del 20%
   datoDescuento = "20%";
   descuento = consumo * 0.2;
+} else if (consumo > 200) {
+  //Practica 2
+  //descuento del 30%
+  datoDescuento = "30%";
+  descuento = consumo * 0.3;
 }
 
 let montoDescuento = consumo - descuento;
@@ -26,8 +43,8 @@ let totalPagar = montoDescuento + iva;
 
 document.write(`<pre>
     Consumo             : ${consumo}
-    Descuento           : ${descuento}
+    Descuento           : ${descuento}      | ${datoDescuento}
     Monto Con Descuento : ${montoDescuento}
-    Iva                 : ${iva}
+    Iva                 : ${iva}            | 19%
     Total a pagar       : ${totalPagar}
 </pre>`);
