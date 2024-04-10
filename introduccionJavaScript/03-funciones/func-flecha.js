898af97e0ece35cea9afa77499acbbdcdeed851d
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-10 15:36:46 -0300

    funcion flecha

---
+++ introduccionJavaScript/03-funciones/func-flecha.js
@@ -0,0 +1,15 @@
+/*function suma(a , b){
+    return a+b;
+}*/
+
+//la funcion flecha simplifica una funcion son para problemas mas sencillos.
+
+const sumar = (a, b) => a + b;
+
+console.log(sumar(20, 30));
+
+const saludar = () => {
+  console.log("Hola mundo!!");
+};
+
+saludar();
