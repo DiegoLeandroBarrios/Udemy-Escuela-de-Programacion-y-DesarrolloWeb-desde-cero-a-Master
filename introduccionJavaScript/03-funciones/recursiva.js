10ada6451207615e7743d3a1eecdf53aaa5a5e79
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-10 15:26:44 -0300

    add recursividad

--- introduccionJavaScript/03-funciones/recursiva.js
+++ introduccionJavaScript/03-funciones/recursiva.js
@@ -1,10 +1,10 @@
 //Las funciones recursivas se ejecutan asi mismas;
 function factorial(n) {
-  console.log(n);
+  //console.log(n);
   if (n > 1) {
     n = n * factorial(n - 1); //aca esta el ejemplo.
   }
-  console.log(n);
+  //console.log(n);
   return n;
 }
-console.log(factorial(3));
+console.log(factorial(5));
