e19f2eebfa672deddaca7883640db6961d60ff86
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-09 21:41:35 -0300

    retornar

--- introduccionJavaScript/03-funciones/funciones.js
+++ introduccionJavaScript/03-funciones/funciones.js
@@ -3,6 +3,11 @@
 
 function saludar(nombre) {
   //   console.log("Hola Mundo, desde una funcion");
-  console.log(`Hola ${nombre} desde una funcion`);
+  //   console.log(`Hola ${nombre} desde una funcion`);
+  return `Hola ${nombre} desde una funcion`; // devuelve un valor.
 }
 saludar("Diego"); //se ejecuta cuando la llamamos y ahora pasamos un Argumento
+const d = saludar("Diego"); //con esto podemos saludar a varias personas o sea se puede reutilizar
+const a = saludar("Alex");
+console.log(d);
+console.log(a);
