1ff32886e74031a727b1164b5e3b1369d1af4820
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-12 18:46:24 -0300

    add funcionalidad al area del cuadrado 02

--- introduccionJavaScript/04-dom/Figuras-geometricas/js/geometric.js
+++ introduccionJavaScript/04-dom/Figuras-geometricas/js/geometric.js
@@ -1,14 +1,21 @@
 //area y perimetro cuadraro
-let perimetro = (side) => side * 4;
-let area = (side) => lado * side;
-
-console.log(perimetro(4));
+const perimetro = (side) => side * 4;
+const area = (side) => side * side;
+const valueResult = document.getElementById("result"); //generalizamos el valor del Result
 
 function calcularPerimCuadrado() {
   let inputSide = document.getElementById("inputSide"); //obtenemos la direccion del elemento
   let value = Number(inputSide.value); //lo convertimos en number y guardamos el valor en una variable
-  const result = `El perimetro de cuadrado es: ${perimetro(
+  const result = `El perimetro del cuadrado es: ${perimetro(
     value
   )} cm`; //utilizamos esa dicha variable para que haga el calculo con la funcion perimetro y guardamos todo eso en una variable que seria el resultado
-  document.getElementById("result").innerText = result; //mostramos el resultado en el dom en forma de texto utilizando la ubicacion que queremos "id= result"
+  valueResult.innerText = result; //mostramos el resultado en el dom en forma de texto utilizando la ubicacion que queremos "id= result"
+}
+
+function calcularAreaCuadrado() {
+  //reciclamos el interior de la funcion anterior
+  let inputSide = document.getElementById("inputSide");
+  let value = Number(inputSide.value);
+  const result = `El Area del cuadrado es: ${area(value)} cm^2`;
+  valueResult.innerText = result;
 }
