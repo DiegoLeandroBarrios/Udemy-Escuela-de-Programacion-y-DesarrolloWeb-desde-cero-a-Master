1db82a1d35b06d3189ed6804b05a5f40173eb482
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-17 20:32:53 -0300

    patron modular

--- introduccionJavaScript/04-dom/Figuras-geometricas/js/geometric.js
+++ introduccionJavaScript/04-dom/Figuras-geometricas/js/geometric.js
@@ -1,85 +1,105 @@
-//area y perimetro cuadrado
-const perimetro = (side) => side * 4;
-const area = (side) => side * side;
-const valueResult = document.getElementById("result"); //generalizamos el valor del Result
+//Patron modular
 
-function calcularPerimCuadrado() {
-  let inputSide = document.getElementById("inputSide"); //obtenemos la direccion del elemento
-  let value = Number(inputSide.value); //lo convertimos en number y guardamos el valor en una variable
-  const result = `El perimetro del cuadrado es: ${perimetro(
-    value
-  )} cm`; //utilizamos esa dicha variable para que haga el calculo con la funcion perimetro y guardamos todo eso en una variable que seria el resultado
-  valueResult.innerText = result; //mostramos el resultado en el dom en forma de texto utilizando la ubicacion que queremos "id= result"
-}
+const myModule = (() => {
+  //funcion anonima
+  //este patron modular nos ayuda para la produccion, esto noas ayudaa que sea mas privado y no puedan modificar.
+  "use strict"; //verifica el codigo de manera estricta y muestra el error por consola.
+  // let nombre = "Diego";
+  // console.log(nombre);
 
-function calcularAreaCuadrado() {
-  //reciclamos el interior de la funcion anterior
-  let inputSide = document.getElementById("inputSide");
-  let value = Number(inputSide.value);
-  const result = `El Area del cuadrado es: ${area(value)} cm^2`;
-  valueResult.innerText = result;
-}
+  //area y perimetro cuadrado
+  const perimetro = (side) => side * 4;
+  const area = (side) => side * side;
+  const valueResult = document.getElementById("result"); //generalizamos el valor del Result
 
-//perimettro y area de triangulo
+  function calcularPerimCuadrado() {
+    let inputSide = document.getElementById("inputSide"); //obtenemos la direccion del elemento
+    let value = Number(inputSide.value); //lo convertimos en number y guardamos el valor en una variable
+    const result = `El perimetro del cuadrado es: ${perimetro(
+      value
+    )} cm`; //utilizamos esa dicha variable para que haga el calculo con la funcion perimetro y guardamos todo eso en una variable que seria el resultado
+    valueResult.innerText = result; //mostramos el resultado en el dom en forma de texto utilizando la ubicacion que queremos "id= result"
+  }
 
-const perimetroTriangulo = (side1, side2, side3) =>
-  side1 + side2 + side3;
-const areaTriangulo = (base, altura) => base * altura;
+  function calcularAreaCuadrado() {
+    //reciclamos el interior de la funcion anterior
+    let inputSide = document.getElementById("inputSide");
+    let value = Number(inputSide.value);
+    const result = `El Area del cuadrado es: ${area(value)} cm^2`;
+    valueResult.innerText = result;
+  }
 
-function calcularPerimTriangulo() {
-  let inputSide1 = document.getElementById("inputSideTriangulo1");
-  let inputSide2 = document.getElementById("inputSideTriangulo2");
-  let inputSide3 = document.getElementById("inputSideTriangulo3");
-  let value1 = Number(inputSide1.value);
-  let value2 = Number(inputSide2.value);
-  let value3 = Number(inputSide3.value);
-  const result = `El perimetro del triangulo es ${perimetroTriangulo(
-    value1,
-    value2,
-    value3
-  )} cm`;
-  valueResult.innerText = result;
-}
+  //perimettro y area de triangulo
 
-function calcularAreaTriangulo() {
-  let base = document.getElementById("base");
-  let altura = document.getElementById("altura");
-  let valueBase = Number(base.value);
-  let valueAltura = Number(altura.value);
-  const result = `El area del triangulo es ${areaTriangulo(
-    valueBase,
-    valueAltura
-  )} cm^2`;
-  valueResult.innerText = result;
-}
+  const perimetroTriangulo = (side1, side2, side3) =>
+    side1 + side2 + side3;
+  const areaTriangulo = (base, altura) => base * altura;
 
-//Radio circulo diametro,perimetro, area
+  function calcularPerimTriangulo() {
+    let inputSide1 = document.getElementById("inputSideTriangulo1");
+    let inputSide2 = document.getElementById("inputSideTriangulo2");
+    let inputSide3 = document.getElementById("inputSideTriangulo3");
+    let value1 = Number(inputSide1.value);
+    let value2 = Number(inputSide2.value);
+    let value3 = Number(inputSide3.value);
+    const result = `El perimetro del triangulo es ${perimetroTriangulo(
+      value1,
+      value2,
+      value3
+    )} cm`;
+    valueResult.innerText = result;
+  }
 
-const diametro = (radio) => radio * 2;
-const areaCirculo = (radio) => 3.14 * (radio * radio);
-const perimetroCirculo = (radio) => 3.14 * diametro(radio);
+  function calcularAreaTriangulo() {
+    let base = document.getElementById("base");
+    let altura = document.getElementById("altura");
+    let valueBase = Number(base.value);
+    let valueAltura = Number(altura.value);
+    const result = `El area del triangulo es ${areaTriangulo(
+      valueBase,
+      valueAltura
+    )} cm^2`;
+    valueResult.innerText = result;
+  }
 
-function calcularDiametro() {
-  let inputRadio = document.getElementById("radio");
-  let valueRadio = Number(inputRadio.value);
-  const result = `El diametro es: ${diametro(valueRadio)} cm`;
-  valueResult.innerText = result;
-}
+  //Radio circulo diametro,perimetro, area
 
-function calcularAreaCirculo() {
-  let inputRadio = document.getElementById("radio");
-  let valueRadio = Number(inputRadio.value);
-  const result = `El area del circulo es: ${areaCirculo(
-    valueRadio
-  )} cm^2`;
-  valueResult.innerText = result;
-}
+  const diametro = (radio) => radio * 2;
+  const areaCirculo = (radio) => 3.14 * (radio * radio);
+  const perimetroCirculo = (radio) => 3.14 * diametro(radio);
 
-function calcularPerimCirculo() {
-  let inputRadio = document.getElementById("radio");
-  let valueRadio = Number(inputRadio.value);
-  const result = `El perimetro del circulo es: ${perimetroCirculo(
-    valueRadio
-  )} cm`;
-  valueResult.innerText = result;
-}
+  function calcularDiametro() {
+    let inputRadio = document.getElementById("radio");
+    let valueRadio = Number(inputRadio.value);
+    const result = `El diametro es: ${diametro(valueRadio)} cm`;
+    valueResult.innerText = result;
+  }
+
+  function calcularAreaCirculo() {
+    let inputRadio = document.getElementById("radio");
+    let valueRadio = Number(inputRadio.value);
+    const result = `El area del circulo es: ${areaCirculo(
+      valueRadio
+    )} cm^2`;
+    valueResult.innerText = result;
+  }
+
+  function calcularPerimCirculo() {
+    let inputRadio = document.getElementById("radio");
+    let valueRadio = Number(inputRadio.value);
+    const result = `El perimetro del circulo es: ${perimetroCirculo(
+      valueRadio
+    )} cm`;
+    valueResult.innerText = result;
+  }
+  return {
+    //esta no es la mejor manera de hacerlo.
+    cpc: calcularPerimCuadrado,
+    cac: calcularAreaCuadrado,
+    cpt: calcularPerimTriangulo,
+    cat: calcularAreaTriangulo,
+    cdc: calcularDiametro,
+    cpcir: calcularPerimCirculo,
+    cacir: calcularAreaCirculo,
+  };
+})();
