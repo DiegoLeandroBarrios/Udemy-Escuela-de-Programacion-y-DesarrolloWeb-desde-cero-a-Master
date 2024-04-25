568dc32c878be3f44a04114122f978fab3be2812
DiegoLeandroBarrios <dieguinfriky@gmail.com>
2024-04-25 19:59:54 -0300

    creando registros con thunder client

--- api-rest-express/src/api.js
+++ api-rest-express/src/api.js
@@ -6,11 +6,14 @@ const app = express(); //tengo una app creada con express.
 
 const port = 3000; //creamos un puerto.
 
+app.use(express.json()); //convertimos los formatos json
+
 mongoose.connect(
   "mongodb+srv://diegobarrios:1234@udemycurso.daxhkrt.mongodb.net/api-db?retryWrites=true&w=majority&appName=UdemyCurso"
 ); //esta es la coneccion a la base de datos
 
-app.get("/api/", user.list);
+app.get("/api/", user.list); //llamamos al handler 1
+app.post("/api/", user.create); //llamamos al handler2
 
 app.listen(port, () => {
   console.log(`El ejemplo se esta ejecutando en el puerto: ${port}`);
--- api-rest-express/src/user.handler.js
+++ api-rest-express/src/user.handler.js
@@ -5,5 +5,12 @@ const User = {
     response.status(200); //nos tiraria un 200 si esta todo ok
     response.send(users); //respondemos al cliente con send devolvemos un mensaje
   }, //nuestro primer handler
+  create: async (request, response) => {
+    const user = new Users(request.body); //creamos un objeto para que capture los que nos envia el cliente
+    const saveUser = await user.save(); //aqui lo guardamos.
+    //console.log(request.body); //capturamos el cuerpo que nos da el cliente.
+    response.status(201); //Dice que esta todo ok
+    response.send(saveUser._id); //devolvemos el id para que el cliente vea que esta en la BD
+  }, //con este handler recibimos lo que nos envia el cliente.
 };
 module.exports = User; //exportamos
